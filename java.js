79555dd668e6795613a06135cdab307c3ee2a41e
Richardt97 <54861755+Richardt97@users.noreply.github.com>
2020-09-04 20:53:18 -0500

    changes

--- java.js
+++ java.js
@@ -1,6 +1,24 @@
 function addNumbers (a, b) {
-    var addedTotal= a + b;
-    alert (addedTotal)
+     var addedTotal= a + b;
+    return addedTotal;
 }
 
-addNumbers(15, 12);
+var addNumbers2= addNumbers(500, 849);
+alert (addNumbers2);
+
+
+
+var heroName= prompt ("who is your favorite hero in My Hero Academia");
+
+switch(heroName) {
+    case "all might": 
+    alert ("Plus Ultra!");
+    break;
+
+    case "deku":
+    alert ("Number one! OP")
+    break;
+
+    default:
+        alert ("Watch Anime!");
+}
