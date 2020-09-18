ecd48260378e94b9522a33bcd09bf4c7bdfed5e9
Richardt97 <54861755+Richardt97@users.noreply.github.com>
2020-09-18 16:32:48 -0500

    new assignment

--- java.js
+++ java.js
@@ -22,3 +22,32 @@ switch(heroName) {
     default:
         alert ("Watch Anime!");
 }
+
+var mydemo = document.getElementById("demoparent");
+
+console.log(mydemo.childNodes.length) ;
+
+
+var mydemotags = document.getElementsByTagName("li") ;
+
+console.log ("number of li links: "+ mydemotags.length);
+
+
+var linksinmenu = mydemo.getElementsByTagName("li");
+
+console.log ("numbver of li items in menu:" + linksinmenu.length) ;
+
+var mycomments = document.getElementById("commentary") ;
+
+mycomments.setAttribute ("align", "right") ;
+
+mycomments.setAttribute ("style", "color: green") ;
+
+var newitem = document.createElement ("li") ;
+
+var newtext = document.createTextNode ("Hey, this wasnt here before!")
+
+mydemo.appendChild(newitem) ;
+newitem.appendChild(newtext) ;
+
+
